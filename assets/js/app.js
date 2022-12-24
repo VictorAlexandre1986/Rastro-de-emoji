be183c9f9bb4d1f301dce809238d2fc6ff9c8681
VictorAlexandreBragaRibeiro1986 <victoralexandre29051986@gmail.com>
2022-12-24 15:34:29 -0300

    finalizado

---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+let images = ['😀','😁','😂','🤣','😃','😄','😆','😉','😋','😛'];
+
+
+
+document.addEventListener('mousemove',function(e){
+	let body = document.querySelector('body');
+	let emoji = document.createElement('span');
+	let x = e.offsetX;
+	let y = e.offsetY;
+
+	emoji.style.left = x+'px';
+	emoji.style.top = y+'px';
+
+	let icon = images[Math.floor(Math.random() * images.length)];
+	emoji.innerText = icon;
+
+	let size = Math.random() * 30;
+	emoji.style.fontSize = 5 +size+'px'
+	
+	let max = 0;
+	let min = 200;
+	let randomValue = Math.floor(Math.random() * ((max + 1) - min) + min);
+
+	emoji.style.transform = 'translateX('+ randomValue +'px)';
+	body.appendChild(emoji);
+
+	setTimeout(function(){
+		emoji.remove();
+	},1000);
+})
